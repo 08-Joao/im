60c68fa3c1899244620833025bcd0fd3cda7bde1
08-Joao <joao.victorva08@gmail.com>
2025-04-23 18:08:25 -0300

    Modelagem das tabelas User e Property, junto com seus annotations e relacionamento de tabelas

--- src/main/java/com/im/BackendCore/BackendCoreApplication.java
+++ src/main/java/com/im/BackendCore/BackendCoreApplication.java
@@ -2,8 +2,9 @@ package com.im.BackendCore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BackendCoreApplication {
 
 	public static void main(String[] args) {
